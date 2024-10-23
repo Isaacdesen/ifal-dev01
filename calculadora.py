ca6c7a08ceb2c3f65fd7a137fd6d34d423086f7d
Isaacalbert <iaso1@aluno.ifal.edu.br>
2024-10-23 13:53:14 -0300

    atividade da calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,4 +1,10 @@
-x = input("informe o primeiro valor:")
-y = input("informe o segundo valor:")
-z = int(x) * int(y)
-print(f"o resultado de {x} x {y} = {z}")
+#pegar os valores do usuário
+x = int(input("informe o primeiro numero:"))
+y = int(input("informe o segundo numero:"))
+
+# calcula e Exibe o resultado
+print(f"A soma de {x} + {y} é {x + y}")
+print(f"A subtração de {x} - {y} é {x - y}")
+print(f"A multiplicação de {x} * {y} é {x * y}")
+print(f"A divisão de {x} / {y} é {x / y}")
+print(f"O resto de {x} % {y} é {x % y}")
