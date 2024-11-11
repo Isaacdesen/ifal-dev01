08d38f8c64348dfb63082c9e9af5341243b353db
Isaacalbert <iaso1@aluno.ifal.edu.br>
2024-11-11 16:40:05 -0300

    nota.py

---
+++ nota.py
@@ -0,0 +1,11 @@
+pontuação = int(input("pontuação:"))
+if pontuação >= 90:
+    print("nota: A")
+elif pontuação >= 80:
+    print("Nota: B")
+elif pontuação >= 70: 
+    print("nota: C")
+elif pontuação >= 60:
+    print("Nota: D")
+else:
+    print("Nota F")
